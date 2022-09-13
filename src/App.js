871414e42ca46d6c76aa1c52d5fc6b3487d392c0
Igor Dragun <draggun.ia@gmail.com>
2022-09-13 16:48:30 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Header } from './components/Header';
+import { Shop } from './components/Shop';
+import { Footer } from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Shop />
+      <Footer />
+    </>
+  );
+}
+
+export default App;

ca9a33bf42b206a23f52f2d7322fe1608f575179
Igor Dragun <draggun.ia@gmail.com>
2022-09-14 13:17:07 +0300

    add repo link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ function Header() {
   return (
     <nav className="indigo accent-4">
       <div className="nav-wrapper">
-        <a href="!#" className="brand-logo">
+        <a href="/" className="brand-logo">
           React Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="!#">Repo</a>
+            <a href="https://github.com/dragun-igor/shop">Repo</a>
           </li>
         </ul>
       </div>

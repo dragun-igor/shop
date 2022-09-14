a2ef2446b8051e239a91849fbc6bb4efe34ca97e
Igor Dragun <draggun.ia@gmail.com>
2022-09-14 13:14:12 +0300

    change image

--- src/components/GoodsItem.jsx
+++ src/components/GoodsItem.jsx
@@ -1,21 +1,28 @@
 function GoodsItem(props) {
-  const { id, name, description, price, full_background, addToBasket } = props;
+  const { id, name, description, price, image, addToBasket } = props;
 
   return (
     <div className="card" id={id}>
       <div className="card-image">
-        <img src={full_background} alt={name} />
+        <img src={image} alt={name} />
       </div>
       <div className="card-content">
         <span className="card-title">{name}</span>
         <p>{description}</p>
       </div>
       <div className="card-action">
-        <button className="btn" onClick={() => addToBasket({
-          id,
-          name,
-          price,
-        })}>Купить</button>
+        <button
+          className="btn"
+          onClick={() =>
+            addToBasket({
+              id,
+              name,
+              price,
+            })
+          }
+        >
+          Купить
+        </button>
         <span className="right" style={{ fontSize: '1.6rem' }}>
           {price} ₽
         </span>
